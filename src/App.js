89b1f2386012986bfc124ccc17e3657cbf0e798a
Jonathan Kirkpatrick <jone.kirkpatrick@gmail.com>
2021-02-25 17:47:06 -0800

    first commit, working on <TodoList/>

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TodoList from "./component/TodoList/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ToDo App</h1>
+      <h1>Input</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/TodoList/TodoList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <h3>Todo Title</h3>
+          <p>Sample ToDo</p>
+        </li>
+        <li>
+          <h3>Todo Title</h3>
+          <p>Sample ToDo</p>
+        </li>
+        <li>
+          <h3>Todo Title</h3>
+          <p>Sample ToDo</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
