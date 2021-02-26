3261fb36bf0b7383271f1b03505db29983b9a934
Jonathan Kirkpatrick <jone.kirkpatrick@gmail.com>
2021-02-25 21:10:12 -0800

    added inital load context GET, setting up DELETE

--- src/context/todo-context.js
+++ src/context/todo-context.js
@@ -13,17 +13,31 @@ const TodoContextProvider = (props) => {
     { title: "test todo", body: "test body", id: "3w" },
   ]);
 
-  const newHandler = (newTodo) => {
-    setAllTodos([...allTodos, newTodo]);
-  };
+  useLayoutEffect(() => {
+    fetch("https://react-hooks-8671f-default-rtdb.firebaseio.com/todos.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resData) => {
+        const nearlyTodos = Object.entries(resData);
+        const finalTodos = [];
+        for (let each of nearlyTodos) {
+          let item = { ...each[1], name: each[0] };
+          finalTodos.push(item);
+        }
+
+        setAllTodos(finalTodos);
+      });
+  }, []);
 
   const deleteHandler = (id) => {
+    fetch("");
     const updatedTodos = allTodos.filter((todo) => todo.id !== id);
     setAllTodos(updatedTodos);
   };
 
   const randIndex = () => {
-    return Math.floor(Math.random() * 100 * Math.random());
+    return Math.floor(Math.random() * 100 + Math.random() * 555555);
   };
   const postTodo = (event, data) => {
     event.preventDefault();
@@ -34,13 +48,15 @@ const TodoContextProvider = (props) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ ...data, id: randIndex() }),
+    }).then((res) => {
+      console.log(res);
+      setAllTodos([...allTodos, { ...data, id: randIndex() }]);
     });
   };
 
   return (
     <TodoContext.Provider
       value={{
-        new: (newTodo) => newHandler(newTodo),
         delete: (id) => deleteHandler(id),
         post: (event, data) => postTodo(event, data),
         todos: allTodos,
